60c23326c94271f7ee105e8e2db6e352fe4d3cb2
sanjay <0703sanjay5165@gmail.com>
2024-07-30 21:54:13 +0530

    new

--- 8.Dictionary/iii.Remove Duplicates.py
+++ 8.Dictionary/iii.Remove Duplicates.py
@@ -1,5 +1,17 @@
 #Remove all duplicates from a given sentence
     #Input : Python is great and Java is also great
-    #Output : is also Java Python and great
+    #Output : python is great and java also
+
+sentance = input("Enter the Sentence: ")
+
+input = sentance.split()
 
 input_dict = {}
+
+for word in input:
+    input_dict[word] = None
+
+    end = " ".join(input_dict.keys())
+
+
+print("Sentence after removing duplicates: ",end)
