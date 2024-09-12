e7914b68048d714cfcedaa117f3b791a417aa7db
sanjay <0703sanjay5165@gmail.com>
2024-09-12 19:09:12 +0530

    new

---
+++ 9.Funtions/i.Area of Triangle.py
@@ -0,0 +1,10 @@
+# Write a Python program that defines a method to calculate the area of a rectangle
+
+
+def triangle():
+    A = int(input("Enter the Base: "))
+    B = int(input("Enter the Height:"))
+    Area = 0.5*A*B
+    print (Area,".square units")
+
+triangle()
---
+++ 9.Funtions/ii.Sm of all elements.py
@@ -0,0 +1,5 @@
+#Create a method that accepts a list of integers 
+# and returns the sum of all the elements in the array.
+
+def sum():
+    
